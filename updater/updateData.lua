58c763921e2c74bd32ea184549afc16061e183b0
Ryan Pavlik <rpavlik@iastate.edu>
2012-08-22 17:14:21 -0500

    Show the count deltas after loading each file, as well.
    Signed-off-by: Ryan Pavlik <rpavlik@iastate.edu>

--- updater/updateData.lua
+++ updater/updateData.lua
@@ -6,6 +6,8 @@ local CorrectionDatabase = require "CorrectionDatabase"
 
 data = CorrectionDatabase()
 
+local lastTypos = 0
+local lastCorrections = 0
 for _, fn in ipairs(arg) do
 	print("Loading file", fn)
 	local f = assert(io.open(fn, "r"))
@@ -16,8 +18,16 @@ for _, fn in ipairs(arg) do
 		data:add(line)
 	end
 	f:close()
-	print("Current incorrect spelling and corrections counts:", data:getCounts())
-	print("\n")
+	local typos, corrections = data:getCounts()
+	print("Current incorrect spelling and corrections counts:")
+	print( ("%d [+%d] incorrect spellings, %d [+%d] corrections\n"):format(
+			typos,
+			typos - lastTypos,
+			corrections,
+			corrections - lastCorrections)
+	)
+	lastTypos = typos
+	lastCorrections = corrections
 end
 
 local f = assert(io.open("outputdict.txt", "wb"))
